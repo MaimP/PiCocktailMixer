a335a8ef32829415ab6ed82224b28af0ab281b74
Mika Uthmann <uthmann2@web.de>
2020-06-15 14:37:07 +0200

    testet relay

---
+++ Relay_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import time
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+
+DEVICE = 0x20 # Device Adresse (A0-A2)
+IODIRA = 0x00 # Pin Register fuer die Richtung
+IODIRB = 0x01 # Pin Register fuer die Richtung
+OLATA = 0x14
+OLATB = 0x15 # Register fuer Ausgabe (GPB)
+GPIOA = 0x12 # Register fuer Eingabe (GPA)
+
+GPIO.setmode(GPIO.BCM)
+# expander | Relay | hex
+#-------------------------
+#  0    01          0x80
+#  1    02          0x40
+#  2    03          0x20
+#  3    04          0x10
+#  4    05          0x8
+#  5    06          0x4
+#  6    07          0x2
+#  7    08          0x1
+
+# initiate list with pin gpio pin numbers
+
+gpioList = [0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1]
+
+for i in gpioList:
+    #GPIO.setup(i, GPIO.OUT)
+    #GPIO.output(i, GPIO.HIGH)
+    bus.write_byte_data(DEVICE,OLATA,i)
+
+# Sleep time variables
+
+sleepTimeShort = 0.2
+sleepTimeLong = 0.1
+
+# MAIN LOOP =====
+# ===============
+
+try:
+    while True:
+        for i in gpioList:
+            #GPIO.output(i, GPIO.LOW)
+            bus.write_byte_data(DEVICE,OLATA,i)
+            time.sleep(sleepTimeShort);
+            bus.write_byte_data(DEVICE,OLATA,0)
+            time.sleep(sleepTimeLong);
+
+
+# End program cleanly with keyboard
+except KeyboardInterrupt:
+    print " Quit"
+
+    # Reset GPIO settings
+
+    GPIO.cleanup()
