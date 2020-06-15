9b497a0156eda8125019f4cc34463e724b940630
Mika Uthmann <uthmann2@web.de>
2020-06-15 19:00:20 +0200

    a

--- ultraschallsensor.py
+++ ultraschallsensor.py
@@ -62,8 +62,8 @@ def distanz():
     entfernungsmesserGpioAus()
 
     while bus.read_byte_data(DEVICE,GPIOB) & 0b00000010 == 0b00000000: #2
-    StartZeit = time.time()
-    print("Startzeit wurde erfasst.")
+        StartZeit = time.time()
+        print("Startzeit wurde erfasst.")
 #    if bus.read_byte_data(DEVICE,GPIOB) & 0b01000000 == 0b00000000: #2
 #        StartZeit = time.time()
 #        print("Startzeit wurde erfasst.")
