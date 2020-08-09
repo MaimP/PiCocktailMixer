2baf085ab1e10500ca305618ae1bcfde076e03b2
Mika Uthmann <uthmann2@web.de>
2020-08-09 22:33:12 +0200

    re

--- server.py
+++ server.py
@@ -167,7 +167,7 @@ from geventwebsocket.handler import WebSocketHandler
 host = "192.168.178.72"
 port = 8080
 
-server = WSGIServer((host, reloader=True, port), app,
+server = WSGIServer((host, port), app,
                     handler_class=WebSocketHandler)
 print "access @ http://%s:%s/websocket.html" % (host,port)
 server.serve_forever()
