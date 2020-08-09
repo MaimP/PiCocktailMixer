5af45b3f2d78850a6912128f29cf5487443062cf
Mika Uthmann <uthmann2@web.de>
2020-08-09 20:38:04 +0200

    ws

--- server.py
+++ server.py
@@ -14,6 +14,29 @@ global hoehe
 global entfernung
 global progress
 
+#!/usr/bin/python
+
+import json
+from pymongo import Connection
+from gevent import monkey; monkey.patch_all()
+from time import sleep
+
+app = Bottle()
+
+@app.route('/websocket')
+def handle_websocket():
+    wsock = request.environ.get('wsgi.websocket')
+    if not wsock:
+        abort(400, 'Expected WebSocket request.')
+    while True:
+        try:
+            message = wsock.receive()
+            wsock.send("Your message was: %r" % message)
+            sleep(3)
+            wsock.send("Your message was: %r" % message)
+        except WebSocketError:
+            break
+
 @route('/static/:path#.+#', name='static')
 def static(path):
     return static_file(path, root='static')
@@ -137,28 +160,17 @@ def order():
     ap.orderManager()
     print("nach app.app")
 
-from bottle import request, Bottle, abort
-app = Bottle()
-
-@app.route('/websocket')
-def handle_websocket():
-    wsock = request.environ.get('wsgi.websocket')
-    if not wsock:
-        abort(400, 'Expected WebSocket request.')
-
-    while True:
-        try:
-            message = wsock.receive()
-            wsock.send("Your message was: %r" % message)
-        except WebSocketError:
-            break
 
 from gevent.pywsgi import WSGIServer
-from geventwebsocket import WebSocketError
-from geventwebsocket.handler import WebSocketHandler
-server = WSGIServer(("0.0.0.0", 8080), app,
+from geventwebsocket import WebSocketHandler, WebSocketError
+
+host = "192.168.178.72"
+port = 8080
+
+server = WSGIServer((host, port), app,
                     handler_class=WebSocketHandler)
+print "access @ http://%s:%s/websocket.html" % (host,port)
 server.serve_forever()
 
 
-run(host='192.168.178.72', reloader=True, port=8080, debug=True)
+#run(host='192.168.178.72', reloader=True, port=8080, debug=True)
