8ee350f49822ad590acc33168c87c454ca080656
Mika Uthmann <uthmann2@web.de>
2020-08-15 14:15:43 +0200

    update ultraschall future

--- ultraschallsensor.py
+++ ultraschallsensor.py
@@ -27,7 +27,7 @@ def distanz():
     GPIO.setup(GPIO_ECHO, GPIO.IN)
 
     now = time.time()
-    future = now + 0.5
+    future = now + 0.1
     # setze Trigger auf HIGH
     GPIO.output(GPIO_TRIGGER, True)
     # debug
