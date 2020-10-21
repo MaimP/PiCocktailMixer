887f4d76e735f238eea2ca7c2d596c335acbcc84
Mika Uthmann <uthmann2@web.de>
2020-10-21 20:31:48 +0200

    test vol imagees

--- volume_cup.py
+++ volume_cup.py
@@ -10,22 +10,22 @@ def cupArray():
     {
         'name': 'Ikea Pokal groß',
         'volume': '350',
-        'image': ''
+        'image': '/home/pi/picocktailmixer/pic_vol/pokal-glas-klarglas__0908826_PE609409_S5.JPG'
     },
     {
         'name': 'Ikea Pokal klein',
         'volume': '270',
-        'image': ''
+        'image': '/home/pi/picocktailmixer/pic_vol/pokal-glas-klarglas__0896474_PE609414_S5.JPG'
     },
     {
         'name': 'Ikea Vardagen',
         'volume': '310',
-        'image': ''
+        'image': '/home/pi/picocktailmixer/pic_vol/vardagen-glas-klarglas__0896318_PE609373_S5.JPG'
     },
     {
         'name': 'Red Cup',
         'volume': '470',
-        'image': ''
+        'image': '/home/pi/picocktailmixer/pic_vol/plastikbecher-rot-360ml-50-einheiten.jpg'
     }
     ]
 
